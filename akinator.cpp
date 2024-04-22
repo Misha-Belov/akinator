78a6292e21e07b899730df26633f74c98c1543c6
Misha-Belov <59139007+Misha-Belov@users.noreply.github.com>
2024-04-22 18:05:01 +0300

    old

--- akinator.cpp
+++ akinator.cpp
@@ -118,7 +118,10 @@ void game(node * root)
 
 void save_tree(node * root, FILE * file)
 {
-    fprintf(file, "{%s", root->content);
+    if (root->no == NULL && root->yes == NULL)
+        fprintf(file, "{%s{}", root->content);
+    else
+        fprintf(file, "{%s", root->content);
 
     if (root->yes != NULL)
         save_tree(root->yes, file);
@@ -233,4 +236,257 @@ int queue_pop(struct stack* pstk)
     pstk->data++;
     pstk->size--;
     return *(pstk->data - 1);
+}
+
+
+void comparison(node * root)
+{
+    bool is_found = 0;
+
+    char * answer_1 = (char *) calloc(100, sizeof(char));
+    char * answer_2 = (char *) calloc(100, sizeof(char));
+
+    stack def_stack_1;
+    stack def_stack_2;
+    stack comp_stack;
+
+    stack_construct(&def_stack_1, 10);
+    stack_construct(&def_stack_2, 10);
+    stack_construct(&comp_stack, 10);
+
+    printf("input first character:\n");
+    scanf("%s", answer_1);
+    person_search(root, answer_1, &def_stack_1, &is_found);
+
+    is_found = 0;
+    
+    printf("input second character:\n");
+    scanf("%s", answer_2);
+    person_search(root, answer_2, &def_stack_2, &is_found);
+
+    stack_compare(&def_stack_1, &def_stack_2, &comp_stack);
+    print_compare(&comp_stack, &def_stack_1, &def_stack_2, answer_1, answer_2, root);
+
+
+    stack_destruct(&def_stack_1);
+    stack_destruct(&def_stack_2);
+    stack_destruct(&comp_stack);
+
+    free(answer_1);
+    free(answer_2);
+}
+
+void stack_compare(stack * def_stack_1, stack * def_stack_2, stack * comp_stack)
+{
+    int i = 0;
+    while (def_stack_1->data[i] == def_stack_2->data[i])
+    {
+        if (def_stack_1->data[i] == 1)
+        {
+            push(comp_stack, 1);
+        }
+        else
+        {
+            push(comp_stack, 0);
+        }
+        i++;
+    }
+}
+
+//-------------------------------
+
+void print_compare(stack * comp_ctack, stack * def_stack_1, stack * def_stack_2, char * name_1, char * name_2, node * root)
+{
+    bool is_first = 1;
+    bool is_common = 0;
+
+    if(!strcmp(name_1, name_2))
+    {
+        printf("it is one person");
+        return;
+    }
+    
+    if (comp_ctack->size != 0)
+    {
+        is_common = 1;
+        printf("%s is like %s in that they're both ", name_1, name_2);
+    }
+
+    while (comp_ctack->size != 0)
+    {
+        if (is_first == 1)
+        {
+            printf("%s", root->content);
+            is_first = 0;
+        }
+        else if(comp_ctack->size == 1)
+        {
+            printf(" and %s", root->content);
+        }
+        else
+        {
+            printf(", %s", root->content);
+        }
+
+        if (queue_pop(comp_ctack) == 1)
+        {
+            root = root->yes;
+        }
+        else
+        {
+            root = root->no;
+        }
+        
+        queue_pop(def_stack_1);
+        queue_pop(def_stack_2);
+    }
+
+    if(is_common == 1)
+    {
+        printf(". ");
+        printf("But %s is ", name_1);
+    }
+    else
+    {
+        printf("%s is ", name_1);
+    }
+    print_definition(root, def_stack_1);
+    printf(", and %s is ", name_2);
+    print_definition(root, def_stack_2);
+    printf(".\n");
+}
+
+//--------------------------
+
+void person_insert(node * root, char * ans, char * que)
+{
+    node * new_false = (node *) calloc(1, sizeof(node));
+    node * new_true = (node *) calloc(1, sizeof(node));
+    char tmp[100];
+
+    printf("who was your character?\n");
+    scanf("%s", ans);
+    printf("how is your character diifferent from %s?\n", root->content);
+    scanf("%s", que);
+
+    root->no = new_false;
+    root->yes = new_true;
+    strcpy(tmp, root->content);
+    strcpy(root->content, que);
+    strcpy(new_false->content, tmp);
+    strcpy(new_true->content, ans);
+}
+
+void definition(node * root)
+{
+    node * tmp_root = root;
+    bool is_found = 0;
+    char * answer = (char *) calloc(100, sizeof(char));
+
+    stack def_stack;
+    stack_construct(&def_stack, 10);
+
+    printf("input your character:\n");
+    scanf("%s", answer);
+
+    person_search(root, answer, &def_stack, &is_found);
+    printf("%s is ", answer);
+    print_definition(root, &def_stack);
+    printf(".\n");
+
+    stack_destruct(&def_stack);
+    free(answer);
+
+    root = tmp_root;
+}
+
+void print_definition(node * root, stack * def_stack)
+{
+    int direction = 0;
+    bool is_first = 1;
+
+
+    while (def_stack->size != 0)
+    {
+        direction = queue_pop(def_stack);
+        
+        if (def_stack->size != 0)
+        {
+            is_first = 0;
+
+            if (direction == 1)
+            {
+                if (def_stack->size == 1)
+                {
+                    printf("%s ", root->content);
+                }
+                else
+                {
+                    printf("%s, ", root->content);
+                }
+                root = root->yes;
+            }
+            else
+            {
+                if (def_stack->size == 1)
+                {
+                    printf("not %s ", root->content);
+                }
+                else
+                {
+                    printf("not %s, ", root->content);
+                }
+                root = root->no;
+            }
+        }
+        else
+        {
+            if (direction == 1)
+            {
+                if(is_first == 1)
+                {
+                    printf("%s", root->content);
+                }
+                else
+                {
+                    printf("and %s", root->content);
+                }
+            }
+            else
+            {
+                if(is_first == 1)
+                {
+                    printf("not %s", root->content);
+                }
+                else
+                {
+                    printf("and not %s", root->content);
+                }
+            }
+        }
+    }
+}
+
+void person_search(node * root, char * person_name, stack * def_stack, bool * is_found)
+{    
+    if (!strcmp(root->content, person_name))
+    {
+        *is_found = 1;
+        printf("charachter found\n");
+        return;
+    }
+    if (root->yes != NULL && *is_found == 0)
+    {
+        push(def_stack, 1);
+        person_search(root->yes, person_name, def_stack, is_found);
+        if (*is_found == 1) return;
+        pop(def_stack);
+    }
+    if (root->no != NULL && *is_found == 0)
+    {
+        push(def_stack, 0);
+        person_search(root->no, person_name, def_stack, is_found);
+        if (*is_found == 1) return;
+        pop(def_stack);
+    }
 }
